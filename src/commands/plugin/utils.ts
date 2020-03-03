0e6c9716db7181693cd76085e1ec8bc727082c1a
carrotWu <542425997@qq.com>
2020-03-03 22:42:54 +0800

    feat: 增加npm包安装插件

--- src/commands/plugin/utils.ts
+++ src/commands/plugin/utils.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import * as fse from 'fs-extra'
+import { execSync } from 'child_process'
 import { isArray } from '../../utils/checktype';
 import { getPluginPathWithPluginName } from '../../utils/getPluginFile';
 import throwHandleError from '../../utils/errorHandler/error';
@@ -49,6 +50,11 @@ export function getDownloadGitRepoPath(pluginGitPath: string): string {
   throwHandleError('请检查插件地址是否正确，插件安装地址目前只支持[github]和[gitlab]')
 }
 
+/**
+ * 根据git地址下载相应的插件
+ * @param {string} pluginGitPath 插件地址
+ * @returns {Promise<string>}
+ */
 export async function downloadPluginByGit(pluginGitPath: string): Promise<string> {
   // 获取插件名称
   const pluginName = getPluginNameByPluginGitPath(pluginGitPath)
@@ -67,7 +73,36 @@ export async function downloadPluginByGit(pluginGitPath: string): Promise<string
   try {
     await downloadPluginByPath(downloadPath, downloadPluginPath)
     success(`插件[${pluginName}]下载成功，正在安装插件所需依赖`)
-    // todo
+    autoPackageJsonInstall(downloadPluginPath, 'yarn')
+    success(`插件[${pluginName}]安装成功，你可以在项目中使用该插件！`)
+    return downloadPluginPath
+  } catch (e) {
+    throwHandleError(e.message)
+  }
+}
+
+// todo
+export async function installPluginByNpm(npmName: string) {
+  // 首先检测当前npm包是否存在
+  try {
+    execSync(`npm info ${npmName}`)
+  } catch (e) {
+    throwHandleError(`包${npmName.bold}不存在与npm registry中！`)
+  }
+  // 先检查插件是否已经安装了
+  if (getPluginPathWithPluginName(npmName)) {
+    throwHandleError('插件已在plugin目录下存在，请勿重新安装')
+  }
+  // 未安装 安装插件
+  loading(`下载插件[${npmName}]中，请耐心等候...`)
+  // 安装插件的地址
+  const pluginsDirectionPath = resolve(__dirname, '../../../plugins')
+  const downloadPluginPath = resolve(pluginsDirectionPath, npmName)
+  // 插件下载前先创建目录结构
+  await fse.ensureDir(downloadPluginPath)
+  try {
+    await downloadPluginByPath(downloadPath, downloadPluginPath)
+    success(`插件[${pluginName}]下载成功，正在安装插件所需依赖`)
     autoPackageJsonInstall(downloadPluginPath, 'yarn')
     success(`插件[${pluginName}]安装成功，你可以在项目中使用该插件！`)
     return downloadPluginPath
